040a4a76e8fddacd82350d159668dcc6e7b49158
ShubhamP100 <117891766+ShubhamP100@users.noreply.github.com>
2022-11-12 20:10:17 +0530

    Add files via upload

---
+++ FIT.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int x;
+	    cin>>x;
+	    int a=5;
+	    cout<<(a*2*x)<<endl;
+	    
+	    
+	}
+	
+	return 0;
+}
