bf773ad4ffd13309e6d170c92b45387d1bbdb52a
ShubhamP100 <117891766+ShubhamP100@users.noreply.github.com>
2022-11-13 23:46:05 +0530

    Add files via upload

---
+++ WTRMIXING.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int a,b,x,y;
+	    cin>>a>>b>>x>>y;
+	    if(a==b){
+	    cout<<"YES"<<endl;
+	    }else if(a<b && (b-a) <=x)
+	    cout<<"YES"<<endl;
+	    else if(a>b && (a-b)<=y)
+	    cout<<"YES"<<endl;
+	    else 
+	    cout<<"NO"<<endl;
+	}
+	return 0;
+}
