773692fe57ccb857dd211a519bfba3a509a16a4f
ShubhamP100 <117891766+ShubhamP100@users.noreply.github.com>
2022-11-15 23:38:32 +0530

    Add files via upload

---
+++ MUFFINS3.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int n;
+	    cin>>n;
+	    int final=n/2+1;
+	    cout<<final<<endl;
+	}
+	return 0;
+}
