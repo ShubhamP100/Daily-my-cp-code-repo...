6046795b4223591c51eb999a46ac69000bef151f
ShubhamP100 <117891766+ShubhamP100@users.noreply.github.com>
2022-11-27 00:35:33 +0530

    Add files via upload

---
+++ HOTCOLD.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int c;
+	    cin>>c;
+	    if(c>20){
+	        cout<<"HOT"<<endl;
+	    }else{
+	        cout<<"COLD"<<endl;
+	    }
+	}
+	return 0;
+}
